49fd63061e39e6c046a626f05c8a8da3f47e4330
Francisco Lezana Ramos <betoramos_20@hotmail.com>
2022-03-02 16:48:20 -0600

    ADD: First repo

---
+++ APITest/APITest/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APITest.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<>)
+        {
+
+        }
+    }
+}
---
+++ APITest/APITest/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITest.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+
+        public string Nombre { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
